47e28e5e7c8535fe58943998bea73b894c50cfa7
Rene <rene.zobus@gmail.com>
2021-05-25 18:52:27 +0200

    added a reset button

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+const body = document.querySelector("body")
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".resetBtn")
 
+resetBtn.addEventListener("click", () => {
+    resetBoard();
+});
 
 function createBoard() {
-  
+
     for(let i = 0; i <= 255; i++) {
         const square = document.createElement("div");
-        square.classList.add("square")
-        square.addEventListener("mouseover", changeColor)
+        square.classList.add("square");
+        square.addEventListener("mouseover", changeColor);
         container.appendChild(square);
-    }
-}
+    };
+};
 
 function changeColor(e) {
-    console.log("cock")
     e.target.style.backgroundColor = "pink";
+};
+
+function resetBoard() {
+    const square = document.getElementsByClassName("square");
+    for( let i = 0; i<square.length; i++) {
+        square[i].style.backgroundColor = "rgba(201, 201, 201, 0.692)"
+    }
 };
 
 createBoard();
