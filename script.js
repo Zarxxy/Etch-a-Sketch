26fc86a249b4a9aed56c2ca915e0cfdb985e23dd
Rene <rene.zobus@gmail.com>
2021-05-26 13:38:18 +0200

    added userinput fpr boardsize

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 const body = document.querySelector("body")
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".resetBtn")
+const randomBtn = document.querySelector(".randomBtn")
+
 
 resetBtn.addEventListener("click", () => {
     resetBoard();
 });
 
-function createBoard() {
+randomBtn.addEventListener("click", () => {
+    randomColor();
+})
 
-    for(let i = 0; i <= 255; i++) {
+function createBoard(rowSize) {
+    let boardSize = rowSize * rowSize;
+    let squareHight = (400 / rowSize) + "px";
+    let squareWidth = (400 / rowSize) + "px";
+
+    for(let i = 0; i < boardSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.addEventListener("mouseover", changeColor);
+        square.style.height = squareHight;
+        square.style.width = squareWidth;
         container.appendChild(square);
-    };
-};
+    }
+}
+
 
 function changeColor(e) {
     e.target.style.backgroundColor = "pink";
 };
 
-function resetBoard() {
-    const square = document.getElementsByClassName("square");
-    for( let i = 0; i<square.length; i++) {
-        square[i].style.backgroundColor = "rgba(201, 201, 201, 0.692)"
-    }
-};
 
-createBoard();
+function resetBoard() {
+    let rowSize = prompt("How many squares per side? (Maximum: 100)");
+    rowSize = parseInt(rowSize);
+
+    if(isNaN(rowSize)) {
+        window.alert("Please enter a positive number between 1 and 100.");
+        return;
+    } else if (rowSize > 100) {
+        window.alert("Please enter a positive number between 1 and 100.");
+        return;
+    } else if (rowSize <= 0) {
+        window.alert("Please enter a positive number between 1 and 100.");
+        return;
+    }
+
+    while (container.firstChild) container.removeChild(container.firstChild); 
+    createBoard(rowSize);
+}
+
+createBoard(16);
